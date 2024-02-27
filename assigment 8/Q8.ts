880fc07481c261418203700de4a6d6c975bd2889
Tanveer-Ahmed-Ansari <159915506+Tanveer-Ahmed-Ansari@users.noreply.github.com>
2024-02-27 22:13:29 +0500

    Add files via upload

---
+++ assigment 8/Q8.ts
@@ -0,0 +1,13 @@
+// 8.You should create four lines that look like this:
+
+// console.log(5 + 3)
+
+// Your output should simply be four lines with the number 8 appearing once on each line.
+// addition
+console.log(5+3);
+// subtration
+console.log(12-4);
+// multiplaction
+console.log(4*2);
+// division
+console.log(16/2);
